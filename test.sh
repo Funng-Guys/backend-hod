a89d467dade291cb7e81d12e73353449b1da7043
olofthesnowman <43708321+olofthesnowman@users.noreply.github.com>
2024-09-16 21:16:53 +0200

    Improved test script

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-curl -v -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer 2efhWdawJNO24THU9D2WQ" localhost:3000
+#!/bin/bash
+
+printf "===\nCorrect headers:\n"
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer 2efhWdawJNO24THU9D2WQ" localhost:3000
+
+printf "\n\n===\nUnauthorized headers:\n"
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer failiure" localhost:3000
